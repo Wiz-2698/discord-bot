6ca7f500a18305b079ec97f14a4d4fee4261702e
Wiz-2698 <wayne654852@gmail.com>
2026-03-08 22:02:15 +0800

    Update redeem_code.py

--- redeem_code.py
+++ redeem_code.py
@@ -52,6 +52,7 @@ SALT = "tB87#kPtkxqOS2"
 HTTP_HEADER = {
     "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 }
 
 r = requests.Session()
